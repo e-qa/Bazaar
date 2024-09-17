a7928f2895e4e81d233a33a4dfab70ef3f958d02
e-qa <eliqarayev835@gmail.com>
2024-09-17 15:14:07 +0400

    add `404` not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
