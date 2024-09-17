6dd5c1bdd90d7d3f3a4da547452b74f90caa3e9b
e-qa <eliqarayev835@gmail.com>
2024-09-17 14:25:16 +0400

    add daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <button className="btn">Hello daisyUI</button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
