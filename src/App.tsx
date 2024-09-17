03652384b9cabee4ae462f7f5907eb575d87f38c
e-qa <eliqarayev835@gmail.com>
2024-09-17 15:10:28 +0400

    created a route for the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <div className="align-element">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
